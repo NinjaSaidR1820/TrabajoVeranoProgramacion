c1c5e806dd5035d97bef62890337455f464fcd28
JADPA04 <JADPA04@JADPA04>
2022-02-03 10:44:13 -0600

    Agregar archivos de proyecto.

---
+++ TrabajoCursoProgramacion/Clases/CEmpleado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoCursoProgramacion.Clases
+{
+    public class CEmpleado
+    {
+        private double sueldo;
+        private double impuesto;
+
+
+        public void setSueldo(double psueldo)
+        {
+
+            sueldo = psueldo;
+
+        }
+
+        public double getSueldo()
+        {
+            return sueldo;
+        }
+
+        public double getImpuesto()
+        {
+
+            return impuesto;
+        }
+
+        public void CalcularImpuesto()
+        {
+
+            impuesto = sueldo * 0.16;
+        }
+
+    }
+}
---
+++ TrabajoCursoProgramacion/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrabajoCursoProgramacion.Clases;
+
+namespace TrabajoCursoProgramacion
+{
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+        double sueldo = 10000.50;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double impuesto = 0.0;
+            double sueldo = 0.0;
+
+
+
+
+            CEmpleado Juan = new CEmpleado();
+
+            Juan.setSueldo(10000.0);
+
+            sueldo = Juan.getSueldo();
+
+            Juan.CalcularImpuesto();
+
+
+            impuesto = Juan.getImpuesto();
+
+            MessageBox.Show("Impuesto: " + impuesto.ToString()+
+                "\r\n Sueldo: "+sueldo.ToString());
+            
+
+        }
+
+       
+
+    }
+}
